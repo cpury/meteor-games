a6292d7dea9e2a01a8fa5eb61c5ea2cdedd16c36
Max <phraine@gmail.com>
2016-01-09 22:47:31 +0100

    Option to pass options to publish

--- packages/game-base/server.js
+++ packages/game-base/server.js
@@ -7,53 +7,53 @@ Meteor.startup(function () {
 });
 
 // Publish all games
-Meteor.publish("games.all", function () {
-  return Games.find({});
+Meteor.publish("games.all", function (options) {
+  return Games.find({}, options);
 });
 
 // Publish all games that were played by a given user
-Meteor.publish("games.byPlayer", function (userId) {
+Meteor.publish("games.byPlayer", function (userId, options) {
   gameInstances = GameInstances.find({players: userId}, {fields: {gameId: 1}});
   gameIds = gameInstances.map(function (doc) { return doc.gameId; });
-  return Games.find({_id: {$in: gameIds}});
+  return Games.find({_id: {$in: gameIds}}, options);
 });
 
 // Publish all games that are actively being played by a given user
-Meteor.publish("games.active.byPlayer", function (userId) {
+Meteor.publish("games.active.byPlayer", function (userId, options) {
   gameInstances = GameInstances.find(
     {players: userId, status: "playing"},
     {fields: {gameId: 1}
   });
   gameIds = gameInstances.map(function (doc) { return doc.gameId; });
-  return Games.find({gameId: {$in: gameIds}});
+  return Games.find({gameId: {$in: gameIds}}, options);
 });
 
 // Publish all game instances
-Meteor.publish("gameInstances.all", function () {
-  return GameInstances.find({});
+Meteor.publish("gameInstances.all", function (options) {
+  return GameInstances.find({}, options);
 });
 
 // Publish a single game instance with the given id
-Meteor.publish("gameInstances.single", function (gameInstanceId) {
-  return GameInstances.find({_id: gameInstanceId});
+Meteor.publish("gameInstances.single", function (gameInstanceId, options) {
+  return GameInstances.find({_id: gameInstanceId}, options);
 });
 
 // Publish game instances that are of the given game
-Meteor.publish("gameInstances.byGame", function (gameId) {
-  return GameInstances.find({gameId: gameId});
+Meteor.publish("gameInstances.byGame", function (gameId, options) {
+  return GameInstances.find({gameId: gameId}, options);
 });
 
 // Publish game instances with the given author id
-Meteor.publish("gameInstances.byAuthor", function (authorId) {
-  return GameInstances.find({authorId: authorId});
+Meteor.publish("gameInstances.byAuthor", function (authorId, options) {
+  return GameInstances.find({authorId: authorId}, options);
 });
 
 // Publish game instances where the given user is a player
-Meteor.publish("gameInstances.byPlayer", function (userId) {
-  return GameInstances.find({players: userId});
+Meteor.publish("gameInstances.byPlayer", function (userId, options) {
+  return GameInstances.find({players: userId}, options);
 });
 
 // Publish currently active game instances where the given user is a player
-Meteor.publish("gameInstances.active.byPlayer", function (userId) {
-  return GameInstances.find({players: userId, status: "playing"});
+Meteor.publish("gameInstances.active.byPlayer", function (userId, options) {
+  return GameInstances.find({players: userId, status: "playing"}, options);
 });
