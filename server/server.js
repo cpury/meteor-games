7bacde407211f6d7b03c07d78cbf1d192f23b8c2
Max <phraine@gmail.com>
2016-01-10 22:54:38 +0100

    Game fixtures: Upsert for easy updates

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 // Defines server initialization code and publishes collections to the client
 
 Meteor.startup(function () {
-  // Load game fixtures if games collection empty
-  if (Games.find({}).count() === 0) {
-    var games = JSON.parse(Assets.getText('games_fixtures.json'));
-    _.each(games, function(game) {
-      Games.insert(game);
-    });
-  }
+  // Load game fixtures and upsert
+  var games = JSON.parse(Assets.getText('games_fixtures.json'));
+  _.each(games, function(game) {
+    Games.upsert(
+      {gameId: game.gameId},
+      {$set: game}
+    );
+  });
 });
